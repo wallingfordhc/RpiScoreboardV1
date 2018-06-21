cc51e166ef5c737ef8fec26c6d3bb2a2efe6b523
David Shannon <dzshanno@yahoo.com>
2018-06-21 22:59:59 +0100

    added first attempt to set home score from mqtt message

--- Scoreboard.py
+++ Scoreboard.py
@@ -229,7 +229,7 @@ class MatrixDisplay:
                 away_score = away
                 #length = graphics.DrawText(offscreen_canvas, scorefont, awayxpos, awayypos, hometextcolour, away_score)
                 image = Image.open("number6.png")
-                offscreen_canvas.SetImage(image, -5)
+                offscreen_canvas.SetImage(image, 10,10)
 
             if home:
                 home_score = home
