faa543f9c617902f7d61cdb6a2924e2409ed619d
David Shannon <dzshanno@yahoo.com>
2018-06-22 21:55:19 +0100

    added first attempt to set home score from mqtt message

--- ScoreboardV2.py
+++ ScoreboardV2.py
@@ -8,162 +8,162 @@
 import paho.mqtt.client as mqtt
 from rgbmatrix import RGBMatrix, RGBMAtrixOptions
 from rgbmatrix import graphics
-from PIL import image
+from PIL import Image
 import time
-import datetime
-import argparse
-import sys
+from datetime import datetime
+from dateutils import parser
 
 import config
 
+
 class MyMQTTClient(mqtt.Client):
-  
-    def __init__(self,host,port,keepalive_interval,topic)
-        self.connect(host,port,keepalive_interval)
+
+    def __init__(self, host, port, keepalive_interval, topic):
+        self.connect(host, port, keepalive_interval)
+        self.subscribe(topic, 0)
         self.loop_start()
- 
-  
-    def on_connect(self,mosq, obj, rc):
+
+    def on_connect(self, mosq, obj, rc):
         print("Connected to broker")
-        
-        
+
     def on_subscribe(self, mosq, obj, mid, granted_qos):
         print("Subscribed to topic: " + str(mid) + " with QoS: " + str(granted_qos))
-        
-        
-    def on_message(self,mosq,obj, msg):
-        print ("Received: " + msg.payload)
-        message_handler(msg)
-        
-        
-    def message_handler(msg):
+
+    def on_message(self, mosq, obj, msg):
+        print("Received: " + msg.payload)
+        self.message_handler(msg)
+
+    def message_handler(self, msg):
         message_verb, message_value = msg.split(';')
-        if message_verb == "homescore"
-            homescore(message_value)
-          
-          
-        #TODO ADD MORE IF BRANCHES
-        
-    def homescore(score):
-        homescorewidget.value = score
-        
-    
+        if message_verb == "homescore":
+            self.homescore(message_value)
+
+        # TODO ADD MORE IF BRANCHES
+
+    def homescore(self, score):
+        homescorewidget.content = score
+
+
 class ScoreboardDisplay:
-  
+
     def __init__(self):
-        
         options = RGBMAtrixOptions()
-        #TODO MOVE TO CONFIG FILE
+        # TODO MOVE TO CONFIG FILE
         options.hardware_mapping = "adafruit-hat"
         options.disable_hardware_pulsing = True
         options.rows = 16
         options.columns = 32
         options.chain_length = 4
-        options.multiplexing = 8 #CORRECT value for 1/4 SCAN PANELS
-        
-        self.matrix = RGBMatrix(options = options)
-      
-class DisplayWidget()
+        options.multiplexing = 8  # CORRECT value for 1/4 SCAN PANELS
 
-    def __init__(self,x,y,xwidth,ywidth,is_visible = True):
+        self.matrix = RGBMatrix(options=options)
+        self.offscreen_canvas = self.matrix.CreateFrameCanvas()
+
+
+class DisplayWidget:
+
+    def __init__(self, parentdisplay, x, y, xwidth, ywidth, is_visible=True):
         self.x = x
         self.y = y
-        self.offscreen_canvas = self.matrix.CreateFrameCanvas()
+        self.xwidth = xwidth
+        self.ywidth = ywidth
         self.is_visible = is_visible
-        self.value = 0
-        
-    def drawtext(text,x,y,colour,font):
-        length = graphic.DrawText(self.offscreen_canvas,self.font,self.x + x,self.y + y, self.colour, text)
-        
-    def showimage(image,x,y):
-        self.offscreen_canvas.SetImage(image,self.x + x,self.y + y)
-        
-    def fillwidget(colour):
-        t = datetime.datetime.now()
-        ms = t.microsecond
-        if ms % 1000 > 500:
-            c=255
-        else:
-            c=0
-        for i in range(self.x,self.x + self.xwidth - 1):
-            for j in range(self.y,self.y + self.ywidth - 1):
-                self.offscreen_canvas.SetPixel(i,j,colour,colour,colour)
-        
-        
-    def displayscore():
-        digits = {'0','number0r.png',
-                 }
-        scoreimage = Image.open(digits[self.value])
-        self.showimage(scoreimage,0,0)
-        
-    def displayclock()
-        t = datetime.datetime.now()
+        self.content = 0
+        self.parentdisplay = parentdisplay
+
+    def showtext(self, text, x, y, font, colour=0):
+        scorefont = graphics.Font()
+        scorefont.LoadFont("/home/pi/fonts/" + font)
+        length = graphics.DrawText(self.parentdisplay.offscreen_canvas, font, self.x + x, self.y + y, colour, text)
+
+    def showimage(self, image, x, y):
+        self.parentdisplay.offscreen_canvas.SetImage(image, self.x + x, self.y + y)
+
+    def fillwidget(self, colour):
+        for i in range(self.x, self.x + self.xwidth - 1):
+            for j in range(self.y, self.y + self.ywidth - 1):
+                self.parentdisplay.offscreen_canvas.SetPixel(i, j, colour, colour, colour)
+
+    def displayscore(self):
+        digits = {'0': "number0r.png",
+                  '1': "number1r.png",
+                  '2': "number2r.png",
+                  '3': "number3r.png",
+                  '4': "number4r.png",
+                  '5': "number5r.png",
+                  '6': "number6r.png",
+                  '7': "number7r.png",
+                  '8': "number8r.png",
+                  '9': "number9r.png",
+                  '10': "number10r.png",
+                  '11': "number11r.png"
+                  }
+
+        scoreimage = Image.open(digits[self.content])
+        self.showimage(scoreimage, 0, 0)
+
+    def displayclock(self):
+        t = datetime.now()
         clocktext = t.strftime('%H:%M:%S')
-        self.showtext(clocktext,0,0)
-        
-    def displaytimer()
-        t = self.value
+        self.showtext(clocktext, 0, 0, "8x13.bdf")
+
+    def displaytimer(self):
+        t = parser.parse(self.content)
         if t.hour == 0:
-            timertext = t.strftine('%M:%S')
+            timertext = t.strftime('%M:%S')
         else:
             timertext = t.strftime('%H:%M:%S')
-        self.showext(timertext,0,0)
-        
-    def displaymessage():
-        self.showtext(self.value,0,0)
-        
-    def displayheartbeat():
-      # FLASH ALL PIXELS ONTHE WIDGET
-      t=datetime.datetime.now()
-      ms = t.micrsecond
-      if ms % 1000 > 500:
-          c=255
-      else:
-          c=0
-          
-      self.fillwidget(c)
-      
-      
-    
-        
+        self.showtext(timertext, 0, 0, "8x13.bdf")
+
+    def displaymessage(self):
+        self.showtext(self.content, 0, 0, "8x13.bdf")
+
+    def displayheartbeat(self):
+        # FLASH ALL PIXELS ONTHE WIDGET
+        t = datetime.now()
+        ms = t.microsecond
+        if ms % 1000 > 500:
+            c = 255
+        else:
+            c = 0
+
+        self.fillwidget(c)
 
 
 # main function
 if __name__ == "__main__":
-  #initialise MQTT broker
-  
-  #initialise MQTT client
-  mqttclient = MyMQTTClient(config.host, config.port, config.keepalive_interval,conf.topic)
-  
-  #initialise Matrix display
-  sb_display = MatrixDisplay()
-  
-  #initialise screen widgets
-  homescorewidget = DisplayWidget(16,16,16,32)
-  awayscorewidget = DisplayWidget(0,16,16,32)
-  clockwidget = DisplayWidget(0,0,64,16)
-  timerwidget = DisplayWidget(0,0,64,16)
-  messagewidget = DisplayWidget(0,0,64,16)
-  heartbeatwidget = DisplayWidget(0,0,1,1)
-  
-  
-  #loop
-  
-  sb_display.offscreen_canvas.clear()
-  #clear the display
-  
-  #set active widgets 
-  homescorewidget.displayscore()
-  awayscorewidget.displayscore()
-  clockwidget.displayclock()
-  timerwidget.displaytimer()
-  heartbeatwidget.displayheartbeat()
-  
-  
-  #wait a short time
-  time.sleep(0.05)
-  
-  #refresh the display
-  sb_display.SwapOnVSync(sb_display.offscreen_canvas)
-  
-  #repeat
+    # initialise MQTT broker
+
+    # initialise MQTT client
+    mqttclient = MyMQTTClient(config.mqtt['host'], config.mqtt['port'], config.mqtt['keepalive_interval'], config.mqtt['topic'])
+
+    # initialise Matrix display
+    sb_display = ScoreboardDisplay()
+
+    # initialise screen widgets
+    homescorewidget = DisplayWidget(sb_display, 16, 16, 16, 32)
+    awayscorewidget = DisplayWidget(sb_display, 0, 16, 16, 32)
+    clockwidget = DisplayWidget(sb_display, 0, 0, 64, 16)
+    timerwidget = DisplayWidget(sb_display, 0, 0, 64, 16)
+    messagewidget = DisplayWidget(sb_display, 0, 0, 64, 16)
+    heartbeatwidget = DisplayWidget(sb_display, 0, 0, 1, 1)
+
+    # loop
+
+    sb_display.offscreen_canvas.clear()
+    # clear the display
+
+    # set active widgets
+    homescorewidget.displayscore()
+    awayscorewidget.displayscore()
+    clockwidget.displayclock()
+    timerwidget.displaytimer()
+    heartbeatwidget.displayheartbeat()
+
+    # wait a short time
+    time.sleep(0.05)
+
+    # refresh the display
+    sb_display.offscreen_canvas.SwapOnVSync(sb_display.offscreen_canvas)
+
+    # repeat
