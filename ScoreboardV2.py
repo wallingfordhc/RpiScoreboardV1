742ec55dcb1a4b137ffa77445a3eabed270fcdf3
David Shannon <dzshanno@yahoo.com>
2018-06-24 19:21:39 +0100

    tidy up

--- ScoreboardV2.py
+++ ScoreboardV2.py
@@ -282,7 +282,7 @@ while True:
     messagewidget.displaymessage()
 
     # wait a short time
-    time.sleep(0.02)
+    time.sleep(0.5)
 
     # refresh the display
     sb_display.offscreen_canvas = sb_display.matrix.SwapOnVSync(sb_display.offscreen_canvas)
